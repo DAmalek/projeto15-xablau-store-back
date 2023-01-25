f1b206a7ac3d2620eac4b6b780b13daa11d71636
Daniel Abdel <danielmalek101@gmail.com>
2023-01-24 23:10:58 -0300

    refactor: files

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-dotenv.config();
-
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+d;
 
 app.listen(process.env.PORT, () => {
   console.log("server rodando a todo vapor!!");
