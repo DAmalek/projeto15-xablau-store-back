090d327a2778321138c5414341d89eb9883f0c92
Daniel Abdel <danielmalek101@gmail.com>
2023-01-24 21:11:32 -0300

    "chore: adjust code for deploy on heroku"

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(5000, () => {
+dotenv.config();
+
+const mongoClient = new MongoClient();
+
+app.listen(process.env.PORT, () => {
   console.log("server rodando a todo vapor!!");
 });
